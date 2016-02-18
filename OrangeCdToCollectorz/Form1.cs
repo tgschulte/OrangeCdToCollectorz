663ced3c19b8323caf6ea4db0c967bb68801551a
Tom Schulte <tsch@plex.com>
2016-02-18 14:41:18 -0500

    1. improved error handling for illegal characters in XML 2. added large, hairy export.oxl to test from

--- OrangeCdToCollectorz/Form1.cs
+++ OrangeCdToCollectorz/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Serialization;
@@ -27,6 +28,14 @@ namespace OrangeCdToCollectorz
         try
         {
           var ser = new XmlSerializer(typeof(OrangeCd.Collection));
+          //using (MemoryStream memoryStream = new MemoryStream(new UnicodeEncoding().GetBytes(xmlText.Replace((char)0x1A, ' '))))
+          //using (XmlTextReader xsText = new XmlTextReader(memoryStream))
+          //{
+          //  xsText.Normalization = true;
+          //  return (T)xs.Deserialize(xsText);
+          //}
+
+
           using (var reader = XmlReader.Create(file))
           {
             orangeCdCollection = (OrangeCd.Collection)ser.Deserialize(reader);
@@ -68,7 +77,7 @@ namespace OrangeCdToCollectorz
                 {
                   // todo: We are not finding the artist in the deserialized object, for instance: Album ID="504B700C9EEAF33B" 
                   music.Artistfirstletter.Displayname = orangeCdAlbumArtist.Name.Substring(0, 1);
-                  
+
                   // default this property, in case we don't find something better in the OrangeCD Artists collection
                   music.Artistfirstletter.Sortname = orangeCdAlbumArtist.Name.Substring(0, 1);
 
@@ -106,13 +115,26 @@ namespace OrangeCdToCollectorz
           richTextBox1.Text += "Output written to." + path;
 
         }
+        catch (XmlException xmlException)
+        {
+          // example: ' ', hexadecimal value 0x02, is an invalid character. Line 1282, position 23.
+          // http://stackoverflow.com/questions/3102604/ignore-exceptions-that-cross-appdomains-when-debugging-in-visual-studio-2010
+          // if this exception is not caught correctly, "There's an option in Visual Studio 2010 called "Break when exceptions cross AppDomain or managed/native boundaries (Managed only)" (under Debugging > General) that, when unchecked, sometimes helps. When it doesn't I need to quit Visual Studio 2010, delete all temporary files, and then try again. Not a very elegant solution, so if anyone have any better ideas, please provide them."
+          richTextBox1.Text += xmlException.Message + Environment.NewLine;
+          richTextBox1.Text += "This is invalid XML. According to the XML specification, all character references must be to characters which are valid. Valid characters are:" + Environment.NewLine;
+          richTextBox1.Text += "#x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]" + Environment.NewLine;
+          richTextBox1.Text += "Remove the offending characters and try again";
+          ; // <-- For debugging use.
+        }
         catch (IOException ioException)
         {
-          richTextBox1.Text += ioException.Message + Environment.NewLine; ; // <-- For debugging use.
+          richTextBox1.Text += ioException.Message + Environment.NewLine;
+          ; // <-- For debugging use.
         }
         catch (Exception exception)
         {
-          richTextBox1.Text += exception.Message + Environment.NewLine; ; // <-- For debugging use.
+          richTextBox1.Text += exception.Message + Environment.NewLine;
+          ; // <-- For debugging use.
         }
       }
       // richTextBox1.Text = result.ToString(); // <-- For debugging use.
